18c37adee071974c3e49fc83c55e2b67a2642db2
AlexandrKletke <klete@mail.ru>
2022-12-08 15:34:19 +0300

    sol of the 66th ex

---
+++ ex66/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("enter M > 0: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("enter N > M: ");
+int N = int.Parse(Console.ReadLine());
+int SumElementsMN (int M, int N)
+{
+    while(M<N)
+        return (M + SumElementsMN (M+1,N));
+    return N;
+}
+
+Console.WriteLine(SumElementsMN(M, N));
