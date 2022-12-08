6dbfb334b168fdc33aa8bf692f1f89b06e9af5a7
AlexandrKletke <klete@mail.ru>
2022-12-08 15:20:14 +0300

    sol of the 64th ex

---
+++ ex64/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.WriteLine("enter N > 0: ");
+int N = int.Parse(Console.ReadLine());
+string NaturNumbers (int N)
+{
+    if(N == 1){
+        return N.ToString();
+    }
+    return (N + ", " + NaturNumbers (N-1));
+}
+
+Console.WriteLine(NaturNumbers(N));
